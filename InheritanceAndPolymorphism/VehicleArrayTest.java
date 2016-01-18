e36e5b5a40edb9bdbca6808aaf945f177672986d
aaronleff <aaronleff3@gmail.com>
2016-01-18 13:51:32 -0700

    Update VehicleArrayTest.java

--- InheritanceAndPolymorphism/VehicleArrayTest.java
+++ InheritanceAndPolymorphism/VehicleArrayTest.java
@@ -5,16 +5,16 @@ public class VehicleArrayTest {
 
 	 Vehicle[] myVehicles = new Vehicle[5];
 	 
-	 //print out if it is calling a Bicycle rather than a generic Vehicle
 	 myVehicles[0] = new Vehicle(120, 4);
 	 myVehicles[1] = new Bicycle(30, 10);
-     myVehicles[2] = new Bicycle(20, 3);
-     myVehicles[3] = new Vehicle();
-     myVehicles[4] = new Bicycle(50, 18);
+     	myVehicles[2] = new Bicycle(20, 3);
+     	myVehicles[3] = new Vehicle();
+     	myVehicles[4] = new Bicycle(50, 18);
 
      System.out.println("My vehicles: ");
 
-     //polymorphic behavior. 'v' variable can call either bicycle or vehicle
+     //I used a "foreach" loop. Because of polymorphism, the proper object will 
+     //be displayed (i.e. a Bicycle or Generic Vehicle)
      for (Vehicle v : myVehicles) {
          System.out.println(v);
      }
